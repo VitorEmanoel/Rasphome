3570673accf0b15d1a9a543e41659b286c4039bf
VitorEmanoel <vitoremanoelamorim@hotmail.com>
2018-08-24 16:43:11 -0300

    fix error on create deamon

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const mongodb = require('./database')
 const fs = require('fs')
 process.title = 'rasphome'
 
+if(process.argv[2] == 'deamon'){
+    fs.writeFile('/etc/init.d/rasphome', fs.readFileSync('./rasphome'), function(err){
+        if(err) throw err
+        console.log('Deamon criado com sucesso!')
+    })
+    process.exit()
+}
+
 process.on("SIGTERM", function(){
     stop()
 })
@@ -16,7 +24,6 @@ function start(){
     mongodb.open()
     fs.writeFile('/var/run/rasphome.pid', process.pid, function(err){
         if(err)throw err
-        console.log('PID registrado com sucesso.')
     })
 }
 
