3a3b4e71f3142fca12980d6e3fc5bf1a77d4fce6
VitorEmanoel <vitoremanoelamorim@hotmail.com>
2018-09-20 10:55:05 -0300

    starting the development of the pin controller

---
+++ pin-controller/index.js
@@ -0,0 +1,13 @@
+const gpio = require("onoff")
+
+module.exports.changeActuator = function(pin, callback){
+
+}
+
+module.exports.startSensorRead = function(pin, callback){
+
+}
+
+module.exports.stopSensorRead = function(pin, callback){
+    
+}
